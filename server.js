0898040ec8755ebf18cb2051a812fdafff7bb756
Tomiwa Obanla <obanlatomiwa@gmail.com>
2021-05-15 12:24:42 +0100

    prepared for deployment

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const serveStatic = require("serve-static");
+
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "../.env" });
+
+const app = express();
+
+//here we are configuring dist to serve app files
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function(req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log(`app is listening on port: ${port}`);
